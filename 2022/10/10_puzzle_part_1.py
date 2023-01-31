5bbd39d66d7f13bc66861ec81247276b7d36740f
kpapezova <papezka.kristyna@gmail.com>
2023-01-31 21:09:43 +0100

    Adition of the calculations

--- 2022/10/10_puzzle_part_1.py
+++ 2022/10/10_puzzle_part_1.py
@@ -1,10 +1,18 @@
 # Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles. What is the sum of these six signal strengths?
 
 cycle_of_interesed = [20, 60, 100, 140, 180, 220]
-cycles = 1
+cycles = 0
+register_X = 1
+signals_strength = 0
 
 with open("10_test_data.txt") as file:
     input = file.readlines()
+
+# def cycles_evaluation(cycle, cycle_of_interesed, value_X):
+#     """Function for evaluation of signal strength"""
+#     if cycle in cycle_of_interesed:
+#         return cycle * value_X
+
 
 
 for line in input:
@@ -15,5 +23,12 @@ for line in input:
         addx, value = line.split(" ")
         value = int(value)
         print("toto je hodnota", value)
-    
-print(cycles)
+        cycles += 1
+        register_X += value
+
+    if cycles in cycle_of_interesed:
+        signals_strength += (cycles*register_X)
+
+        # signals_strength += cycles_evaluation(cycles, cycle_of_interesed, register_X)
+
+print(signals_strength)
