b339e494c2f48482dff5e5cfcc935e297368185a
kpapezova <papezka.kristyna@gmail.com>
2023-01-31 21:50:19 +0100

    Printing of lines to find the bug shows the defect for the multiplification of cycle 220 and registerX

--- 2022/10/10_puzzle_part_1.py
+++ 2022/10/10_puzzle_part_1.py
@@ -1,6 +1,5 @@
 # Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles. What is the sum of these six signal strengths?
 
-cycle_of_interesed = [20, 60, 100, 140, 180, 220]
 cycles = 0
 register_X = 1
 signals_strength = 0
@@ -8,27 +7,37 @@ signals_strength = 0
 with open("10_test_data.txt") as file:
     input = file.readlines()
 
-# def cycles_evaluation(cycle, cycle_of_interesed, value_X):
-#     """Function for evaluation of signal strength"""
-#     if cycle in cycle_of_interesed:
-#         return cycle * value_X
+def cycles_evaluation(cycle, value_X):
+    """Function for evaluation of signal strength"""
+    cycle_of_interesed = [20, 60, 100, 140, 180, 220]
+    if cycle in cycle_of_interesed:
+        return int(cycle * value_X)
 
+    else:
+        return 0
 
 
 for line in input:
     line = line.strip()
     print(line)
     cycles += 1
+    signals_strength += cycles_evaluation(cycles, register_X)
+    print("signal strengt", signals_strength)
+    
     if "addx" in line:
         addx, value = line.split(" ")
         value = int(value)
-        print("toto je hodnota", value)
         cycles += 1
         register_X += value
+        print("cyklus po addx", cycles, "value", value, "x", register_X)
+        signals_strength += cycles_evaluation(cycles, register_X)
+        print("signal strengt", signals_strength)
 
-    if cycles in cycle_of_interesed:
-        signals_strength += (cycles*register_X)
-
-        # signals_strength += cycles_evaluation(cycles, cycle_of_interesed, register_X)
+    # if cycles in cycle_of_interesed:
+    #     print("cycles", cycles)
+    #     print(cycle_of_interesed)
+    #     print("X", register_X)
+    #     signals_strength += (cycles*register_X)
+    #     print("signals, strength", signals_strength)
 
 print(signals_strength)
